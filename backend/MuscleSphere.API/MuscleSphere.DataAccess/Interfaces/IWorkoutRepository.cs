e0b252251029c67a5b574d7a06740b8bdf9c13cf
viksamiles77 <mileskiviktor77@gmail.com>
2025-02-04 01:39:58 +0100

    Create IWorkoutRepository.cs

---
+++ backend/MuscleSphere.API/MuscleSphere.DataAccess/Interfaces/IWorkoutRepository.cs
@@ -0,0 +1,13 @@
+﻿using MuscleSphere.DomainModels.Entities;
+
+namespace MuscleSphere.DataAccess.Interfaces
+{
+    public interface IWorkoutRepository
+    {
+        Task<List<Workout>> GetUserWorkoutsAsync(string userId);
+        Task<List<Workout>> GetWoroutsByDayAsync(string userId, DayOfWeek day);
+        Task AddWorkoutAsync(Workout workout);
+        Task UpdateWorkoutAsync(Workout workout);
+        Task DeleteWorkoutAsync(Workout workout);
+    }
+}
