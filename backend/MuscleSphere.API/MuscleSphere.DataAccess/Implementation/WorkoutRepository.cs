5571cfcb1070215ac8d8980f0885919b5115f6d2
viksamiles77 <mileskiviktor77@gmail.com>
2025-02-04 01:39:54 +0100

    Create WorkoutRepository.cs

---
+++ backend/MuscleSphere.API/MuscleSphere.DataAccess/Implementation/WorkoutRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MuscleSphere.DataAccess.Interfaces;
+using MuscleSphere.DomainModels.Entities;
+
+namespace MuscleSphere.DataAccess.Implementation
+{
+    public class WorkoutRepository : IWorkoutRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WorkoutRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Workout>> GetUserWorkoutsAsync(string userId)
+        {
+            return await _context.Workouts
+                .Where(w => w.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Workout>> GetWoroutsByDayAsync(string userId, DayOfWeek day)
+        {
+            return await _context.Workouts
+                 .Where(w => w.UserId == userId && w.Day == day)
+                 .ToListAsync();
+        }
+
+        public async Task AddWorkoutAsync(Workout workout)
+        {
+            await _context.Workouts.AddAsync(workout);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task UpdateWorkoutAsync(Workout workout)
+        {
+            _context.Workouts.Update(workout);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task DeleteWorkoutAsync(Workout workout)
+        {
+            _context.Workouts.Remove(workout);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
