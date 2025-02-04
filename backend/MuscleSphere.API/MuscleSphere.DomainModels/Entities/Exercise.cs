932a8f7ab2891f184baa5f89f5c8f1d9316405a1
viksamiles77 <mileskiviktor77@gmail.com>
2025-02-04 01:42:39 +0100

    Create Exercise.cs

---
+++ backend/MuscleSphere.API/MuscleSphere.DomainModels/Entities/Exercise.cs
@@ -0,0 +1,15 @@
+﻿namespace MuscleSphere.DomainModels.Entities
+{
+    public class Exercise
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Sets { get; set; } 
+        public int Reps { get; set; } 
+        public double Weight { get; set; }
+
+        public Guid WorkoutId { get; set; }
+        public Workout Workout { get; set; } = null!;
+    
+    }
+}
