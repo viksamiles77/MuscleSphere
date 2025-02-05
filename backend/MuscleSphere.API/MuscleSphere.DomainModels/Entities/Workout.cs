89996d07dbce374a43a12230411e0300d48e9e21
viksamiles77 <mileskiviktor77@gmail.com>
2025-02-05 01:25:43 +0100

    Update Workout.cs

--- backend/MuscleSphere.API/MuscleSphere.DomainModels/Entities/Workout.cs
+++ backend/MuscleSphere.API/MuscleSphere.DomainModels/Entities/Workout.cs
@@ -3,11 +3,11 @@
     public class Workout
     {
         public Guid Id { get; set; }
-        public string Type  { get; set; }
+        public string Type { get; set; } = string.Empty;
         public DayOfWeek Day { get; set; }
         public DateTime Date { get; set; }
         public List<Exercise> Exercises { get; set; } = new();
         public string UserId { get; set; } = string.Empty;
-        public User User { get; set; }
+        public User User { get; set; } = null!;
     }
 }
