ad4536ec6b3f7f4717958037d722ebd2fee11994
viksamiles77 <mileskiviktor77@gmail.com>
2025-02-05 01:26:05 +0100

    Create WorkoutResponseDTO.cs

---
+++ backend/MuscleSphere.API/MuscleSphere.DTO/Workout/WorkoutResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace MuscleSphere.DTO.Workout
+{
+    public class WorkoutResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public DayOfWeek Day { get; set; }
+        public DateTime Date { get; set; }
+        public List<ExerciseResponseDto> Exercises { get; set; } = new();
+    }
+
+    public class ExerciseResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public double Weight { get; set; }
+    }
+}
