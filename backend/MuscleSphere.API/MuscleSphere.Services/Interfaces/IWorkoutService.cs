5e30ed00b88fef2d2aa4cd3b0b1c5ff168529d75
viksamiles77 <mileskiviktor77@gmail.com>
2025-02-05 01:26:42 +0100

    Create IWorkoutService.cs

---
+++ backend/MuscleSphere.API/MuscleSphere.Services/Interfaces/IWorkoutService.cs
@@ -0,0 +1,12 @@
+﻿using MuscleSphere.DTO.Workout;
+
+namespace MuscleSphere.Services.Interfaces
+{
+    public interface IWorkoutService
+    {
+        Task<List<WorkoutResponseDto>> GetUserWorkoutsAsync(string userId);
+        Task<WorkoutResponseDto> AddWorkoutAsync(string userId, WorkoutDto workoutDto);
+        Task<WorkoutResponseDto> UpdateWorkoutAsync(Guid workoutId, string userId, WorkoutDto workoutDto);
+        Task DeleteWorkoutAsync(Guid workoutId, string userId);
+    }
+}
