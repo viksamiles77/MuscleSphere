8a04590db5ac90082ff48af92e65492adb0f9af4
viksamiles77 <mileskiviktor77@gmail.com>
2025-02-05 01:25:51 +0100

    Create AuthenticationDTO.cs

---
+++ backend/MuscleSphere.API/MuscleSphere.DTO/Authentication/AuthenticationDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MuscleSphere.DTO.Authentication
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(20, ErrorMessage = "Username must be between 3 and 20 characters.", MinimumLength = 3)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, ErrorMessage = "Password must be at least 8 characters long.", MinimumLength = 8)]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class LoginDto
+    {
+        public string UsernameOrEmail { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
